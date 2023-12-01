cdb787cf1ef476abf1de3d25704d07fd71aab879
xhuban <damianelias20@aragon.unam.mx>
2023-11-30 20:26:56 -0600

    Se hizo la estructura basica del formulario

--- index.php
+++ index.php
@@ -1,11 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio de sesion</title>
 </head>
+
+
 <body>
-    
+    <div>
+        <form action="" method="post">
+
+            <label for="usuario">Usuario</label>
+            <input type="text" name="usuario" placeholder="Ingresa tu usuario" required>
+
+            <label for="contrasena">Contraseña</label>
+            <input type="password" name="contrasena" placeholder="Ingresa tu contraseña" required>
+
+            <br><br>
+            <button>Ingresar</button>
+            <br><br>
+
+        </form>
+    </div>
+
+
 </body>
+
 </html>
